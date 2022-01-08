f60c2aced78095a82c8c3ce8460d14a0d6690481
Enigma717 <mt88x2@gmail.com>
2022-01-08 21:27:47 +0100

    Cleanup

--- src/main/java/Client/Board.java
+++ src/main/java/Client/Board.java
@@ -8,92 +8,109 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 public class Board extends JFrame implements MouseListener {
-    private static ArrayList<Tile> circles = new ArrayList<>();
-    private static ArrayList<MovableTile> tiles = new ArrayList<>();
-    private boolean containsCircle=false;
+
+
+    private static final ArrayList<Tile> tiles = new ArrayList<>();
+    private static final ArrayList<Pawn> pawns = new ArrayList<>();
+
     private Point point;
+    private Point Temp;
     private Point point2;
+
+    private final int startingPointY = 460;
+    private final int startingPointY2 = 220;
+    private int startingPoint = 500;
+    private int loop1 = 4;
     private int index;
     private int k = 0;
-    private int startingPoint=500;
-    private int startingPoint2=500;
-    private int startingPointY=460;
-    private int startingPointY2=220;
-    private int loop1=4;
-    private int loop2=4;
-    private Point Temp;
-    private boolean Triangle1=true;
-    private boolean Triangle2=false;
-    private boolean Triangle3=false;
-    private boolean Triangle4=true;
-    private boolean Triangle5=false;
-    private boolean Triangle6=true;
+
+    private final boolean Triangle1 = true;
+    private final boolean Triangle2 = true;
+    private final boolean Triangle3 = true;
+    private final boolean Triangle4 = true;
+    private final boolean Triangle5 = true;
+    private final boolean Triangle6 = true;
+    private boolean containsCircle = false;
+
+
     public Board() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000, 600);
-        this.setExtendedState(MAXIMIZED_BOTH);
-        this.setVisible(true);
+        initFrame();
+
         MoveTile mover = new MoveTile();
         getContentPane().addMouseMotionListener(mover);
         getContentPane().addMouseListener(this);
-        for(int i=13; i>=1;i--) {
-            if(i<=4) {
-                drawUsingFunction1(startingPoint,startingPoint2,i,true);
-                startingPoint+=30;
-                startingPoint2+=30;
-            } else {
-                drawUsingFunction1(startingPoint,startingPoint2, i, false);
-                startingPoint+=30;
-                startingPoint2+=30;
-            }
 
+        for(int i=13; i>=1; i--) {
+            if(i<=4) {
+                drawUsingFunction1(startingPoint, i,true);
+            } else {
+                drawUsingFunction1(startingPoint, i,false);
+            }
+            startingPoint += 30;
         }
     }
 
+    private void initFrame() {
+        setTitle("Trylma-WATD");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 600);
+        setExtendedState(MAXIMIZED_BOTH);
+        setVisible(true);
+    }
 
-    public void drawUsingFunction1(int startingPosition, int startingPoint2,int printCount, boolean paintField) {
+    public void drawUsingFunction1(int startingPosition, int printCount, boolean paintField) {
+
         int x = startingPosition;
-        int step=0;
-        int step2=0;
-        int loop=loop1;
+        int step = 0;
+        int step2 = 0;
+        int loop = loop1;
+
         for(int i=0; i<printCount; i++) {
-            point = new Point(x,startingPointY+step);
-            System.out.println(point +" " + point2);
-            point2 = new Point(x,startingPointY2+step2);
-            if(!(i>=9)  || Triangle4) {
+
+            point = new Point(x, (startingPointY + step));
+            System.out.println(point + " " + point2);
+            point2 = new Point(x, (startingPointY2 + step2));
+
+            if(i<9 || Triangle4) {
                 addTile(point);
             }
+
             if(printCount<=4 && !Triangle6) {
-                this.getContentPane().remove(circles.get(k - 1));
-                circles.remove(k-1);
+                getContentPane().remove(tiles.get(k - 1));
+                tiles.remove(k-1);
                 k--;
-                this.validate();
-                this.repaint();
+
+                validate();
+                repaint();
             }
+
             if(!Triangle2 && loop>=1){
-                this.getContentPane().remove(circles.get(k - 1));
-                circles.remove(k-1);
+                getContentPane().remove(tiles.get(k - 1));
+                tiles.remove(k-1);
                 k--;
-                this.validate();
-                this.repaint();
+
+                validate();
+                repaint();
             }
+
             if(i>=9 && Triangle1) {
                 addTile(point2);
             }
+
             if(paintField && Triangle5) {
                 addTile(point2);
             }
+
             if(loop>=1 && Triangle3) {
                 addTile(point2);
             }
-            x+=15;
-            step-=30;
-            step2+=30;
+
+            x += 15;
+            step -= 30;
+            step2 += 30;
             loop--;
         }
         loop1--;
-
-
     }
 
     @Override
@@ -104,14 +121,15 @@ public class Board extends JFrame implements MouseListener {
     @Override
     public void mousePressed(MouseEvent e) {
         Temp = e.getPoint();
-        for (int z=0; z<tiles.size();z++) {
-            if ((tiles.get(z)).containsCircle(new Point(e.getX(), e.getY()))) {
-                containsCircle = true;
-                index=z;
-                Tile temp =  circles.get(z);
-                circles.remove(z);
-                circles.add(temp);
 
+        for (int z=0; z<tiles.size(); z++) {
+            if ((pawns.get(z)).containsCircle(new Point(e.getX(), e.getY()))) {
+                containsCircle = true;
+                index = z;
+                Tile temp = tiles.get(z);
+
+                tiles.remove(z);
+                tiles.add(temp);
             }
         }
     }
@@ -131,6 +149,7 @@ public class Board extends JFrame implements MouseListener {
     public void mouseExited(MouseEvent e) {
 
     }
+
     class MoveTile extends MouseAdapter {
         public void mouseDragged(MouseEvent e) {
             //poruszanie figurą
@@ -139,16 +158,17 @@ public class Board extends JFrame implements MouseListener {
 
             if (containsCircle) {
                 System.out.println(containsCircle);
-                tiles.get(index).translateCircle(TranslateVector, e);
+                pawns.get(index).translateCircle(TranslateVector, e);
             }
         }
     }
-    private void addTile(Point p) {
-        circles.add(new Tile(p));
-        k++;
-        this.getContentPane().add(circles.get(k - 1));
-        this.validate();
-        this.repaint();
-    }
 
+    private void addTile(Point p) {
+        tiles.add(new Tile(p));
+        getContentPane().add(tiles.get(k));
+        k++;
+
+        validate();
+        repaint();
+    }
 }
