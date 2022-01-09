b5243ad5919a7877710e324ebcd9d5ff25fc65b6
RychuKropkaExe <juzek9898@o2.pl>
2022-01-09 10:49:38 +0100

    Adjustments to Board class

---
+++ src/main/java/Client/Board.java
@@ -0,0 +1,283 @@
+package Client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+public class Board extends JFrame implements MouseListener {
+
+
+    private static final ArrayList<Tile> circles = new ArrayList<>();
+    private static final ArrayList<Pawn> pawns = new ArrayList<>();
+    private static final ArrayList<Pawn> movablePawns = new ArrayList<>();
+
+    private Point point;
+    private Point Temp;
+    private Point point2;
+    private Point savedPosition;
+
+    private final int startingPointY = 460;
+    private final int startingPointY2 = 220;
+    private int startingPoint = 500;
+    private int loop1 = 4;
+    private int index;
+    private int k = 0;
+    private int players = 6;
+    private int pawnsCounter= 0;
+    private int startingArm = 2;
+    private int movablePawnsCounter = 0;
+
+    MoveTile mover = new MoveTile();
+
+    private Boolean[] starArm = new Boolean[6];
+    private boolean containsCircle = false;
+
+
+    public Board() {
+        starArm[0] = true;
+        starArm[1] = true;
+        starArm[2] = true;
+        starArm[3] = true;
+        starArm[4] = true;
+        starArm[5] = true;
+        initFrame();
+
+        for(int i=13; i>=1; i--) {
+            if(i<=4) {
+                drawUsingFunction1(startingPoint, i,true);
+            } else {
+                drawUsingFunction1(startingPoint, i,false);
+            }
+            startingPoint += 30;
+        }
+        setNeighbours();
+    }
+
+    private void initFrame() {
+        getContentPane().addMouseMotionListener(mover);
+        getContentPane().addMouseListener(this);
+        setTitle("Trylma-WATD");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 600);
+        setExtendedState(MAXIMIZED_BOTH);
+        setVisible(true);
+    }
+
+    public void drawUsingFunction1(int startingPosition, int printCount, boolean paintField) {
+
+        int x = startingPosition;
+        int step = 0;
+        int step2 = 0;
+        int loop = loop1;
+
+        for(int i=0; i<printCount; i++) {
+
+            point = new Point(x, (startingPointY + step));
+            System.out.println(point + " " + point2);
+            point2 = new Point(x, (startingPointY2 + step2));
+
+            if(i<9 || starArm[3]) {
+                addTile(point);
+                if(i>=9 && (players==2 || players == 3 || players ==6)){
+                    addPawn(point, new Color(89,0,165), 3);
+                    circles.get(k-1).take();
+                }
+            }
+
+            if(printCount<=4) {
+                if(!starArm[5]) {
+                    getContentPane().remove(circles.get(k - 1));
+                    circles.remove(k - 1);
+                    k--;
+                    validate();
+                    repaint();
+                } else if(players==3 || players==4 || players==6) {
+                    addPawn(point, Color.BLUE, 5);
+                    circles.get(k-1).take();
+
+                }
+            }
+
+            if(!starArm[1] && loop>=1){
+                getContentPane().remove(circles.get(k - 1));
+                circles.remove(k-1);
+                k--;
+
+                validate();
+                repaint();
+            } else if(starArm[1] && loop>=1 && (players==6 || players==4 || players==3)) {
+                addPawn(point, Color.GREEN,1);
+                circles.get(k-1).take();
+            }
+
+            if(i>=9 && starArm[0]) {
+                addTile(point2);
+                if(players==2 || players==6) {
+                    addPawn(point2, new Color(199,24,24),0);
+                    circles.get(k-1).take();
+                }
+            }
+
+            if(paintField && starArm[4]) {
+                addTile(point2);
+                if(players==4 || players==6) {
+                    addPawn(point2, Color.YELLOW,4);
+                    circles.get(k-1).take();
+                }
+            }
+
+            if(loop>=1 && starArm[2]) {
+                addTile(point2);
+                if(players==4 || players ==6){
+                    addPawn(point2, new Color(247,19,132),2);
+                    circles.get(k-1).take();
+                }
+            }
+
+            x += 15;
+            step -= 30;
+            step2 += 30;
+            loop--;
+        }
+        loop1--;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        Temp = e.getPoint();
+
+        for (int z=0; z<movablePawns.size(); z++) {
+            if ((movablePawns.get(z)).containsCircle(new Point(e.getX(), e.getY()))) {
+                System.out.println("SIEMA ENIU");
+                containsCircle = true;
+                savedPosition = new Point(movablePawns.get(z).getCircleCenter());
+                index = z;
+                //Tile temp = circles.get(z);
+                getContentPane().remove(movablePawns.get(z));
+                getContentPane().add(movablePawns.get(z),0);
+                return;
+                /*circles.remove(z);
+                circles.add(temp);*/
+            }
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(containsCircle) {
+            Point dropPoint = e.getPoint();
+            for (int z=0; z<circles.size(); z++) {
+                if ((circles.get(z)).containsCircle(dropPoint)) {
+                    /*savedPosition = new Point(movablePawns.get(z).getX(), movablePawns.get(z).getY());
+                    index = z;*/
+                    //Tile temp = circles.get(z);
+                    if(circles.get(z).isTaken){
+                        break;
+                    } else {
+                        getContentPane().remove(movablePawns.get(index));
+                        movablePawns.get(index).setCircleLocation(circles.get(z).getCircleCenter());
+                        circles.get(z).take();
+                        getContentPane().add(movablePawns.get(index), 0);
+                        validate();
+                        repaint();
+                        return;
+                    }
+                }
+                /*circles.remove(z);
+                circles.add(temp);*/
+            }
+                System.out.println("DUPA");
+                getContentPane().remove(movablePawns.get(index));
+                movablePawns.get(index).setCircleLocation(savedPosition);
+                getContentPane().add(movablePawns.get(index),0);
+                validate();
+                repaint();
+        }
+        containsCircle = false;
+        index = -1;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    class MoveTile extends MouseAdapter {
+        public void mouseDragged(MouseEvent e) {
+            //poruszanie figurą
+            Point TranslateVector = new Point(e.getX() - Temp.x, e.getY() - Temp.y);
+            Temp = e.getPoint();
+
+            if (containsCircle) {
+                movablePawns.get(index).translateCircle(TranslateVector, e);
+            }
+        }
+    }
+
+    private void addTile(Point p) {
+        circles.add(new Tile(p));
+        getContentPane().add(circles.get(k));
+        k++;
+        validate();
+        repaint();
+    }
+    private void addPawn(Point p, Color c, int arm) {
+        if(arm==startingArm-1){
+            movablePawns.add(new Pawn(p,c));
+            getContentPane().add(movablePawns.get(movablePawnsCounter),0);
+            movablePawnsCounter++;
+            validate();
+            repaint();
+        } else {
+            pawns.add(new Pawn(p, c));
+            getContentPane().add(pawns.get(pawnsCounter), 0);
+            pawnsCounter++;
+            validate();
+            repaint();
+        }
+    }
+    private void setNeighbours() {
+        for(Tile tile: circles) {
+            int x1 = tile.getX();
+            int y1 = tile.getY();
+            for(Tile possibleNeighbour: circles) {
+                int x2 = possibleNeighbour.getX();
+                int y2 = possibleNeighbour.getY();
+                int dx = x1-x2;
+                int dy = y1-y2;
+                if(dx==30 && dy==0) {
+                    tile.setNeighbour(0, possibleNeighbour);
+                }
+                if(dx==15 && dy==30) {
+                    tile.setNeighbour(1,possibleNeighbour);
+                }
+                if(dx==-15 && dy==30) {
+                    tile.setNeighbour(2,possibleNeighbour);
+                }
+                if(dx == -30 && dy==0) {
+                    tile.setNeighbour(3,possibleNeighbour);
+                }
+                if(dx==-15 && dy==-30) {
+                    tile.setNeighbour(4,possibleNeighbour);
+                }
+                if(dx==15 && dy==-30) {
+                    tile.setNeighbour(5,possibleNeighbour);
+                }
+            }
+            return;
+        }
+    }
+}
